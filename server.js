38e0777bc6d7f202b560fcc02ffdd9234248dded
Kreidina <kav99mail@gmail.com>
2023-07-18 14:38:14 +0300

    start

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+// ezrOAv7dUpiHUL1w
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Anna:ezrOAv7dUpiHUL1w@cluster0.7pwv9k5.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((e) => {
+    console.log(e.message);
+    process.exit(1);
+  });
