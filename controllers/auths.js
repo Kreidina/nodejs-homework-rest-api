ee9411830ee76dd860853577306f494ed11bc374
Kreidina <kav99mail@gmail.com>
2023-07-31 20:32:55 +0300

    mandatory tasks

--- controllers/auths.js
+++ controllers/auths.js
@@ -1,16 +1,20 @@
 const { User } = require("../models/user");
 const { ctrlWrapper, HttpError } = require("../helpers");
 const bcrypt = require("bcrypt");
-// const gravatar = require("gravatar");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
+const avatarDir = path.join(__dirname, "../", "public", "avatars");
+
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  // const avatarURL = gravatar.url(email);
-  // console.log(avatarURL);
+  const avatarURL = gravatar.url(email);
 
   if (user) {
     throw HttpError(409, "Email in use");
@@ -19,8 +23,9 @@ const register = async (req, res) => {
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
-    // avatarURL,
+    avatarURL,
   });
+
   res.status(201).json({
     user: {
       email: newUser.email,
@@ -64,30 +69,39 @@ const current = async (req, res) => {
   res.json({ email, subscription });
 };
 
+const subscription = async (req, res) => {
+  const { _id } = req.user;
+  const result = await User.findByIdAndUpdate(_id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    email: result.email,
+    subscription: result.subscription,
+  });
+};
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const fileName = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarDir, fileName);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatar", fileName);
+  const image = await Jimp.read(resultUpload);
+  image.resize(250, 250);
+  await image.writeAsync(resultUpload);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.json({ avatarURL });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   logout: ctrlWrapper(logout),
   current: ctrlWrapper(current),
+  subscription: ctrlWrapper(subscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
-// const subscription = async (req, res) => {
-//   const { userId } = req.params;
-//   const result = await User.findByIdAndUpdate(userId, req.body, {
-//     new: true,
-//   });
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(result);
-// };
-// subscription: ctrlWrapper(subscription),
-// router.patch(
-//   "/:userId",
-//   authenticate,
-//   isValidId,
-//   validateBody(schemas.subscriptionSchema, "Missing field subscription"),
-//   ctrl.subscription
-// );
-// const subscriptionSchema = Joi.object({
-//   favorite: Joi.boolean().required(),
-// });
